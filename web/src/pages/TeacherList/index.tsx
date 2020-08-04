91b38d990b7d0a7c02a42916c75ad447796246ad
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-03 21:11:18 -0300

    header da pagina teacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+import './styles.css';
 
 export default function TeacherList() {
     return (
-        <h1>List</h1>
+        <div id="page-teacher-list" className="Container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="Voltar"/>
+                    </Link>
+                    <img src={logoImg} alt="Proffy"/>
+                </div>
+
+                <div className="header-content">
+                    <strong>Estes são os proffys disponíveis.</strong>
+                </div>
+            </header>
+        </div>
     )
 }
