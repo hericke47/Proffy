14e710de0cf38e6e6dbb69a3cd055157cd16d832
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-03 21:27:21 -0300

    transformando o header em um componente e fazendo ter que passar a propriedade title

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 export default function TeacherForm() {
     return (
-        <h1>Form</h1>
-    )
+    <div id="page-teacher-form" className="Container">
+        <PageHeader title="Que incrível que você quer dar aulas." />
+     </div>
+ )
 }
