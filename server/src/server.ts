bf5a8ca70bca52ad0cc220e1494df96dda82d22e
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-06 21:10:43 -0300

    rota que por enquando insere users e classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-
+app.use(express.json());
 // Corpo ( Request Body ): Dados para criação ou atualização de um registro
 // Route Params: Identificar qual recurso eu quero atualizar ou deletar
 // Query Params: Paginação, filtros, ordenação
-
-app.get('/', function (request, response) {
-    return response.json({ message: "helo world" });
-});
+app.use(routes);
 
 app.listen(3333);
 
