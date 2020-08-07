f1d931bc53a1e9eb7e2f284364783b2a51ba099f
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-07 00:33:22 -0300

    tabela de schedule pronta junto com a funcionalidades de converter horas para minutos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes = express.Router();
+
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
 routes.post('/classes', async function (request, response) {
     const {
@@ -23,11 +30,24 @@ routes.post('/classes', async function (request, response) {
 
     const user_id = insertedUsersIds[0];
 
-    await db('classes').insert({
+    const insertedClassesIds = await db('classes').insert({
         subject,
         cost,
         user_id,
     });
+
+    const class_id = insertedClassesIds[0];
+
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+            class_id,
+            week_day: scheduleItem.week_day,
+            from: convertHourToMinutes(scheduleItem.from),
+            to: convertHourToMinutes(scheduleItem.to),
+        };
+    })
+
+    await db('class_schedule').insert(classSchedule);
 
     return response.send();
 });
