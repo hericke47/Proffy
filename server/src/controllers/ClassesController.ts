f015386cc4c22263a570e1da63a58395e6f70ec2
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-07 01:07:53 -0300

    listagem com filtros completa

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -10,6 +10,38 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+    async index(request: Request, response: Response) {
+        const filters = request.query;
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        if (!filters.subject || !filters.week_day || !filters.time) {
+            return response.status(400).json({
+                error: 'Missing filters to search classes'
+            })
+        }
+
+        const timeInMunutes = convertHourToMinutes(time);
+
+        const classes = await db('classes')
+            .whereExists(function() {
+                // verificando se existe um horarios disponivel
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')// where raw é o where inteiro
+                    .whereRaw('`class_schedule`. `week_day` = ??', [Number(week_day)])// que seja igual o dia da semana que quero buscar
+                    .whereRaw('`class_schedule`. `from` <= ??', [timeInMunutes])
+                    .whereRaw('`class_schedule`. `to` > ??', [timeInMunutes])
+            })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']); //seleciona todos os dados da tabela calsses e todos os dados da tabela users
+
+        return response.json(classes);
+    }
+
     async create (request: Request, response: Response) {
         const {
             name,
