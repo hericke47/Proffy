5cd187401fd1a9cd84e6c7bb575e65df45137d9c
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-07 01:26:42 -0300

    Finalizando criação de connections e listagem, caso der erro colocar em outro arquivo e rodar a migration e mandar de volta para o github

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 // Corpo ( Request Body ): Dados para criação ou atualização de um registro
 // Route Params: Identificar qual recurso eu quero atualizar ou deletar
