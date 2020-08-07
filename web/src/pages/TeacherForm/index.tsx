77fc9e37cd42587eb85ef21c5070e5ef1554ef7f
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-07 17:47:10 -0300

    auguns componnets e usando options

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
-import './styles.css';
 import Input from '../../components/Input';
-
 import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import './styles.css';
 
 export default function TeacherForm() {
     return (
@@ -20,11 +22,26 @@ export default function TeacherForm() {
                 <Input name="name" label="Nome completo" />
                 <Input name="avatar" label="Avatar" />
                 <Input name="whatsapp" label="Whatsapp" />
+                <Textarea name="bio" label="Biografia" />
             </fieldset>
 
             <fieldset>
                 <legend>Sobre a aula</legend>
-                <Input name="subject" label="Matéria" />
+                <Select 
+                name="subject" 
+                label="Matéria" 
+                options={[
+                    { value: 'Artes', label: 'Artes' },
+                    { value: 'Biologia', label: 'Biologia' },
+                    { value: 'Ciências', label: 'Ciências' },
+                    { value: 'Educação Física', label: 'Educação Física' },
+                    { value: 'Geografia', label: 'Geografia' },
+                    { value: 'História', label: 'História' },
+                    { value: 'Matemática', label: 'Matemática' },
+                    { value: 'Português', label: 'Português' },
+                    { value: 'Química', label: 'Química' },
+                ]}
+                />
                 <Input name="cost" label="Custo da sua hora por aula" />
             </fieldset>
 
