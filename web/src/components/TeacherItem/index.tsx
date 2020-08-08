e46ec4b34d3cae243f2893775f024a5aa14ca268
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-07 22:07:02 -0300

    web finalizado com a parte de contato e de contagem de conexoes

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
 
 export interface Teacher {
@@ -19,6 +20,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
@@ -37,10 +44,10 @@ const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
                     <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                    <button type="button"> 
-                <img src={whatsappIcon} alt="Whatsapp"/>
-                Entrar em Contato
-                </button>
+                <a  target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}> 
+                    <img src={whatsappIcon} alt="Whatsapp"/>
+                    Entrar em Contato
+                </a>
             </footer>
         </article>
     )
