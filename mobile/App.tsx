a1e2af0de239f91f0490aea436a4589a3be04e3f
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-08 02:33:06 -0300

    estrutura inicial mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from './src/pages/GiveClasses/node_modules/react';
-import { AppLoading } from 'expo';
-
-import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
-import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
-import AppStack from './src/routes/AppStack';
+import React from 'react'; 
+import { View,Text } from 'react-native';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Archivo_400Regular,
-    Archivo_700Bold,
-    Poppins_400Regular,
-    Poppins_600SemiBold
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <>
-        <AppStack />
-        <StatusBar style="light" />
-      </>
-    );
-  }
+  return (
+    <>
+    <View >
+      <Text>agadg</Text>
+      <StatusBar style="light" />
+    </View>
+    </>
+  )
 }
