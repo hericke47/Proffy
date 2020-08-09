abbd8c5bab7b7391d024cef97bb6423fcd7e73d3
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-09 02:59:32 -0300

    pagina de Dar Aulas

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 function GiveClasses() {
-  return <View />
+  const { goBack } = useNavigation(); // volta para a tela anterior independente de onde veio
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View  style={styles.container}>
+      <ImageBackground  resizeMode="contain" source={giveClassesBgImage} style={styles.content}>
+        <Text style={styles.title}>Quer ser um Proffy ?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web
+        </Text>
+
+       
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo Bem</Text>
+      </RectButton>
+    </View>
+  )
 }
 
 export default GiveClasses
